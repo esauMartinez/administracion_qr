ac2be22ee9772abed60a5ea71ce829c0b7b8396d
esauMartinez <martinez117esau@gmail.com>
2026-03-24 12:54:20 -0600

    fix: update API baseURL and remove timeout setting

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const apiAdministracionQr = axios.create({
-  baseURL: 'http://192.168.4.18:3000/api-timbres',
-  timeout: 1000,
+  baseURL: 'http://192.168.4.18:5000/api-timbres',
 })
